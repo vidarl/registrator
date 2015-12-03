32f1cbdab803a9f34979b32227edc89c724b6246
Vidar Langseid <vl@ez.no>
2015-12-03 15:58:18 +0100

    Fixed so that ip of etcd can be injected into registrator using environment variavble ETCD_IP

--- run.sh
+++ run.sh
@@ -4,4 +4,9 @@ if [ "$HOST_IP" == "" ]; then
     echo "Warning : The environment variable HOST_IP not set"
 fi
 
-/bin/registrator -ip ${HOST_IP} etcd-tmpl://etcd:4001/services
+if [ "$ETCD_IP" == "" ]; then
+    echo "Warning : The environment variable ETCD_IP not set"
+fi
+
+
+/bin/registrator -ip ${HOST_IP} etcd-tmpl://${ETCD_IP}:4001/services
